51f6af36fc544a60673ba70d9f4e87c954b255be
AdrianFred <adrian989@me.com>
2024-02-14 20:09:25 +0100

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  // now it breaks the imports in the static. fix
+  assetPrefix: "./",
 };
 
 export default nextConfig;
